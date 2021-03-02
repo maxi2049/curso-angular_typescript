4cfff465caf2513e2d1bf0d48326f2fbb95f18a2
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-02 12:46:46 -0300

    Tarea de tipado

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,3 @@
     ===== Código de TypeScript =====
 */
 
-
-const superHeroe ={
-    nombre: 'Spiderman',
-    edad: 30,
-    direccion:{
-        calle: 'Main St',
-        pais: 'Usa',
-        ciudad: 'NY'
-    },
-
-    mostrarDireccion(){
-        return this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais;
-    }    
-}
-
-const direccion = superHeroe.mostrarDireccion();
-console.log(direccion);
-
